40155127e161cfd6e30b2ebe760bf3168fced962
rhamidov-f5 <r.hamidov@f-5.az>
2026-09-11 15:05:34 +0400

    create full crud for tasks

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,10 +1,30 @@
-const tasks = [
-  {title: 'Do homework', done: 'false'},
-  {title: 'Do dishes', done: 'false'},
-  {title: 'Go for a walk', done: 'false'},
-  {title: 'Take medicine', done: 'false'},
-]
+import Task from "../models/task.js"
+import {generateId} from "../utils/helpers.js"
 
-export const getTasks = (req, res) => {
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({})
   res.status(201).json(tasks);
+}
+
+export const createTask = async (req, res) => {
+  const task = new Task({ id: generateId(), title: req.body.title, status: req.body.status })
+  await task.save()
+  res.status(201).json({message: "Task created successfully"})
+}
+
+export const changeTask = async (req, res) => {
+  const id = req.params.id
+  const task = await Task.findOne({ id })
+  task.title = req.body.title || task.title
+  task.status = req.body.status || task.status
+  await task.save();
+
+  res.json({message: "Task was updated successfully"})
+}
+
+export const deleteTask = async (req, res) => {
+  const id = req.params.id
+  await Task.deleteOne({id});
+
+  res.json({message: "Task was deleted successfully"})
 }
